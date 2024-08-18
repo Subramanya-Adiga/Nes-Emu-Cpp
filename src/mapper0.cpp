b81209b6d03eedfa1ec07ba67492cc72eb501430
Subramanya <subramanya.adiga4@protonmail.com>
2024-08-18 22:32:51 +0530

    Mapper0 Added

---
+++ src/mapper0.cpp
@@ -0,0 +1,40 @@
+#include "mapper0.h"
+
+namespace nes_emu {
+mapper0::mapper0(uint8_t prg_bnks, uint8_t chr_bnks)
+    : mapper(prg_bnks, chr_bnks) {}
+
+bool mapper0::cpu_map_read(uint16_t addr, uint32_t &mapped_addr) {
+  if ((addr >= 0x8000) && (addr <= 0xFFFF)) {
+    mapped_addr = addr & (m_program_banks > 1 ? 0x7FFF : 0x3FFF);
+    return true;
+  }
+  return false;
+}
+
+bool mapper0::cpu_map_write(uint16_t addr, uint32_t &mapped_addr,
+                            uint8_t data) {
+  if ((addr >= 0x8000) && (addr <= 0xFFFF)) {
+    mapped_addr = addr & (m_program_banks > 1 ? 0x7FFF : 0x3FFF);
+    return true;
+  }
+  return false;
+}
+bool mapper0::ppu_map_read(uint16_t addr, uint32_t &mapped_addr) {
+  if (addr >= 0x0000 && addr <= 0x1FFF) {
+    mapped_addr = addr;
+    return true;
+  }
+  return false;
+}
+bool mapper0::ppu_map_write(uint16_t addr, uint32_t &mapped_addr) {
+  if (addr >= 0x0000 && addr <= 0x1FFF) {
+    if (m_character_banks == 0) {
+      mapped_addr = addr;
+      return true;
+    }
+  }
+  return false;
+}
+void mapper0::reset() {}
+} // namespace nes_emu
---
+++ src/mapper0.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "mapper.h"
+
+namespace nes_emu {
+class mapper0 : public mapper {
+public:
+  mapper0(uint8_t prg_bnks, uint8_t chr_bnks);
+  virtual ~mapper0() override = default;
+
+  bool cpu_map_read(uint16_t addr, uint32_t &mapped_addr) override;
+  bool cpu_map_write(uint16_t addr, uint32_t &mapped_addr,
+                     uint8_t data) override;
+
+  bool ppu_map_read(uint16_t addr, uint32_t &mapped_addr) override;
+  bool ppu_map_write(uint16_t addr, uint32_t &mapped_addr) override;
+
+  void reset() override;
+};
+} // namespace nes_emu
