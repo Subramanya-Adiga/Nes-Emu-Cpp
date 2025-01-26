88b551ae971428fd0f4430eb8dabd11d13988214
Subramanya <subramanya.adiga4@protonmail.com>
2025-01-26 18:35:42 +0530

    Controller And Dissassembly

--- src/main.cpp
+++ src/main.cpp
@@ -161,7 +161,7 @@ public:
     nes.load_cartridge("nestest.nes");
 
     // Extract dissassembly
-    // mapAsm = nes.cpu.disassemble(0x0000, 0xFFFF);
+    mapAsm = nes.cpu.disassemble(0x0000, 0xFFFF);
 
     // Reset NES
     nes.reset();
@@ -207,15 +207,15 @@ public:
     Clear(olc::DARK_BLUE);
 
     // Sneaky peek of controller input in next video! ;P
-    // nes.controllers[0] = 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::X).bHeld ? 0x80 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::Z).bHeld ? 0x40 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::A).bHeld ? 0x20 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::S).bHeld ? 0x10 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::UP).bHeld ? 0x08 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::DOWN).bHeld ? 0x04 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::LEFT).bHeld ? 0x02 : 0x00;
-    // nes.controllers[0] |= GetKey(olc::Key::RIGHT).bHeld ? 0x01 : 0x00;
+    nes.controllers[0] = 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::X).bHeld ? 0x80 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::Z).bHeld ? 0x40 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::A).bHeld ? 0x20 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::S).bHeld ? 0x10 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::UP).bHeld ? 0x08 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::DOWN).bHeld ? 0x04 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::LEFT).bHeld ? 0x02 : 0x00;
+    nes.controllers[0] |= GetKey(olc::Key::RIGHT).bHeld ? 0x01 : 0x00;
 
     if (GetKey(olc::Key::SPACE).bPressed)
       bEmulationRun = !bEmulationRun;
