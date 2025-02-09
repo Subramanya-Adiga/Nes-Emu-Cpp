b0547b39debba0174e439355cdf387e1152aa46a
Subramanya <subramanya.adiga4@protonmail.com>
2025-02-09 15:02:40 +0530

    Reorganize Code

--- src/frontends/sdl3/application.cpp
+++ src/frontends/sdl3/application.cpp
@@ -4,8 +4,115 @@
 #include <backends/imgui_impl_sdl3.h>
 #include <glad/glad.h>
 
+namespace {
+
+uint32_t create_texture() {
+  uint32_t tex_id{};
+  glGenTextures(1, &tex_id);
+  glBindTexture(GL_TEXTURE_2D, tex_id);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  return tex_id;
+}
+
+void update_texture(uint32_t tex_id, int32_t width, int32_t height,
+                    uint8_t *data) {
+  glBindTexture(GL_TEXTURE_2D, tex_id);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+               GL_UNSIGNED_BYTE, data);
+  glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+} // namespace
+
 namespace sdl3_app {
 SDL3Application::SDL3Application() {
+
+  init_sdl_opengl_imgui();
+
+  nes.load_cartridge("nestest.nes");
+  nes.reset();
+}
+
+void SDL3Application::run() {
+  bool done = false;
+  SDL_Event event;
+
+  std::vector<std::pair<uint16_t, std::string_view>> dissassm;
+  auto dissassm_map = nes.cpu.disassemble(0x0000, 0xFFFF);
+  dissassm.reserve(dissassm_map.size());
+  for (auto &&[k, v] : dissassm_map) {
+    dissassm.emplace_back(k, v);
+  }
+
+  while (!done) {
+    done = process_events(&event);
+
+    if ((SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) != 0U) {
+      SDL_Delay(10);
+      continue;
+    }
+
+    frame_start();
+
+    ImGui::ShowDemoWindow();
+
+    draw_cpu(&nes.cpu, dissassm);
+
+    frame_flush();
+  }
+}
+
+bool SDL3Application::process_events(SDL_Event *event) {
+  bool done = false;
+  while (SDL_PollEvent(event)) {
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (event->type == SDL_EVENT_QUIT) {
+      done = true;
+    }
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
+        event->window.windowID == SDL_GetWindowID(window)) {
+      done = true;
+    }
+  }
+  return done;
+}
+
+void SDL3Application::frame_start() {
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+}
+
+void SDL3Application::frame_flush() {
+  auto &io = ImGui::GetIO();
+  ImGui::Render();
+
+  glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+
+  glClearColor(0.556F, 0.629F, 0.830F, 255.0F);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+  if ((io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) != 0) {
+    auto *backup_window = SDL_GL_GetCurrentWindow();
+    auto *backup_context = SDL_GL_GetCurrentContext();
+    ImGui::UpdatePlatformWindows();
+    ImGui::RenderPlatformWindowsDefault();
+    SDL_GL_MakeCurrent(backup_window, backup_context);
+  }
+
+  SDL_GL_SwapWindow(window);
+}
+
+void SDL3Application::init_sdl_opengl_imgui() {
   if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS |
                 SDL_INIT_GAMEPAD)) {
     initialized = false;
@@ -22,6 +129,7 @@ SDL3Application::SDL3Application() {
     return;
   }
 
+  // Setup OpenGL Context
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -39,6 +147,8 @@ SDL3Application::SDL3Application() {
 
   SDL_GL_MakeCurrent(window, gl_context);
   SDL_GL_SetSwapInterval(1);
+
+  // GLAD Load Protocals
   if (gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress) == 0) {
     initialized = false;
     fmt::print("GLAD Failed To Initialize\n SDL_ERROR:{}\n", SDL_GetError());
@@ -51,6 +161,8 @@ SDL3Application::SDL3Application() {
                (char *)glGetString(GL_VENDOR),
                (char *)glGetString(GL_RENDERER));
   }
+
+  // Init ImGui
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   auto &io = ImGui::GetIO();
@@ -72,67 +184,7 @@ SDL3Application::SDL3Application() {
 
   ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
   ImGui_ImplOpenGL3_Init("#version 130");
-
   initialized = true;
-  nes.load_cartridge("nestest.nes");
-  nes.reset();
-}
-
-void SDL3Application::run() {
-  bool done = false;
-  SDL_Event event;
-  auto &io = ImGui::GetIO();
-
-  std::vector<std::pair<uint16_t, std::string_view>> dissassm;
-  auto dissassm_map = nes.cpu.disassemble(0x0000, 0xFFFF);
-  dissassm.reserve(dissassm_map.size());
-  for (auto &&[k, v] : dissassm_map) {
-    dissassm.emplace_back(k, v);
-  }
-
-  while (!done) {
-    while (SDL_PollEvent(&event)) {
-      ImGui_ImplSDL3_ProcessEvent(&event);
-      if (event.type == SDL_EVENT_QUIT) {
-        done = true;
-      }
-      if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
-          event.window.windowID == SDL_GetWindowID(window)) {
-        done = true;
-      }
-    }
-    if ((SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) != 0U) {
-      SDL_Delay(10);
-      continue;
-    }
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL3_NewFrame();
-    ImGui::NewFrame();
-
-    ImGui::ShowDemoWindow();
-
-    draw_cpu(&nes.cpu, dissassm);
-
-    ImGui::Render();
-
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-
-    glClearColor(0.556F, 0.629F, 0.830F, 255.0F);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-    if ((io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) != 0) {
-      auto *backup_window = SDL_GL_GetCurrentWindow();
-      auto *backup_context = SDL_GL_GetCurrentContext();
-      ImGui::UpdatePlatformWindows();
-      ImGui::RenderPlatformWindowsDefault();
-      SDL_GL_MakeCurrent(backup_window, backup_context);
-    }
-
-    SDL_GL_SwapWindow(window);
-  }
 }
 
 SDL3Application::~SDL3Application() {
--- src/frontends/sdl3/application.hpp
+++ src/frontends/sdl3/application.hpp
@@ -9,6 +9,12 @@ class SDL3Application {
 public:
   SDL3Application();
   ~SDL3Application();
+
+  SDL3Application(const SDL3Application &) = delete;
+  SDL3Application(SDL3Application &&) = delete;
+  SDL3Application &operator=(const SDL3Application &) = delete;
+  SDL3Application &operator=(SDL3Application &&) = delete;
+
   void run();
 
 private:
@@ -16,5 +22,12 @@ private:
   SDL_Window *window = nullptr;
   SDL_GLContext gl_context = nullptr;
   nes_emu::Emulator nes;
+
+  void frame_start();
+  void frame_flush();
+
+  bool process_events(SDL_Event *event);
+
+  void init_sdl_opengl_imgui();
 };
 } // namespace sdl3_app
